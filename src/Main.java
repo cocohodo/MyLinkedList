c08635b494d0fb2271d3b59b485a2db3f1325418
Park SangHyeon <tkrhkrkfn@naver.com>
2023-11-28 12:51:00 +0900

    9과제 1차 완료

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
-package sec02.chap02;
 
 import java.util.Iterator;
 
-class Node {
-    int data;
+class Node<T> {
+    T data;
     Node prev = null;
     Node next = null;
-    Node(int data,Node prev, Node next) {
+    Node(T data,Node prev, Node next) {
         this.data = data;
         this.prev = prev;
         this.next = next;
     }
-    Node(int data) {
+    Node(T data) {
         this.data = data;
     }
 }
-class MyLinkedList implements Iterator {
+class MyLinkedList <T> implements Iterator {
     private Node start = null;
     private Node end = null;
     private Node pointer = null;
@@ -30,20 +29,21 @@ class MyLinkedList implements Iterator {
         pointer = pointer.next;
         return pointer;
     }
-    MyLinkedList(int node) {
-        start = new Node(node,null,null);
-        end = start;
-        pointer = start;
+    MyLinkedList() {
+        start = null;
+        end = null;
+        pointer = null;
     }
     MyLinkedList(Node node) {
         start = node;
         end = node;
         pointer = node;
     }
-    public void add(int added) {
+    public void add (T added) {
         if(start==null) {
             start = new Node(added,null,null);
             end = start;
+            pointer = start;
         }
         else {
             Node newNode = new Node(added,end,null);
@@ -51,14 +51,14 @@ class MyLinkedList implements Iterator {
             end = newNode;
         }
     }
-    public int getIndex(int index) {
-        if(start==null) return -1;
+    public void getIndex(int index) {
+        if(start==null) System.out.println("null");
         Node pointer = start;
         for(int i = 0; i < index; i++) {
-            if(pointer.next == null) return -1;
+            if(pointer.next == null) System.out.println("null");
             pointer = pointer.next;
         }
-        return pointer.data;
+        System.out.println(pointer.data);
     }
     public void delete(int index) {
         Node pointer = start;
@@ -68,9 +68,15 @@ class MyLinkedList implements Iterator {
         }
         // 첫 번째 원소일 경우
         if(pointer.prev==null) {
-            start=null;
-            end=null;
-            return;
+            if(pointer.next==null) {
+                start=null;
+                end=null;
+                return;
+            }
+            else {
+                start = pointer.next;
+                pointer = pointer.next;
+            }
         }
         // 마지막 원소일 경우
         if(pointer.next==null) {
@@ -82,15 +88,15 @@ class MyLinkedList implements Iterator {
         pointer.prev.next = pointer.next;
         pointer.next.prev = pointer.prev;
     }
-    public int getLast() {
-        if(end==null) return -1;
-        return end.data;
+    public void getLast() {
+        if(end==null) System.out.println("null");
+        System.out.println(end.data);
     }
 }
 public class Main {
     public static void main(String[] args) {
-        Node node1 = new Node(5);
-        Node node2 = new Node(2);
+        Node node1 = new Node("string");
+        Node node2 = new Node(true);
         Node node3 = new Node(3);
         Node node4 = new Node(4);
         MyLinkedList my = new MyLinkedList(node1);
@@ -99,9 +105,9 @@ public class Main {
         my.add(2);
         my.delete(1);
         my.add(1);
-        System.out.println(my.getIndex(0));
-        System.out.println(my.getIndex(1));
-        System.out.println(my.getIndex(2));
-        System.out.println(my.getIndex(3));
+        my.getIndex(0);
+        my.getIndex(1);
+        my.getIndex(2);
+        my.getIndex(3);
     }
 }
